74c419c1e065fa5f26cefe028542d38a899da3a1
jiaren <jia.ren7@gmail.com>
2022-08-20 15:48:06 -0700

    initial commit

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var BackgrountView: UIView!
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func BackgroundButtonClicked(_ sender: Any) {
+        BackgrountView.backgroundColor = UIColor.purple
+        
+    }
+    @IBAction func ChangeText(_ sender: Any) {
+        TextLabel.text = "Good Bye"
+    }
 }
 
